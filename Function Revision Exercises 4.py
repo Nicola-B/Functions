0807f480562a5ef40ceeef8e270734ebc5c3950b
Nicola-B <4030@longroad.ac.uk>
2014-12-04 21:51:46 +0100

    commit

---
+++ Function Revision Exercises 4.py
@@ -0,0 +1,19 @@
+#Nicola Batty
+#04/12/2014
+#Function Revision Exorcises 4
+
+def input_fahrenheirt():
+    fahrenheirt = int(input("Please enter the fahrenheirt you whant to convert: "))
+    return fahrenheirt
+
+def calculate_celsius(fahrenheirt):
+    celsius = (fahrenheirt - 32) * (5/9)
+    return celsius
+
+def display_celsius(celsius,fahrenheirt):
+    print("{0:<2}fahrenheirt = {1:>2}celsius".format(fahrenheirt, celsius))
+
+def convert_temps():
+    fahrenheirt = input_fahrenheirt()
+    celsius = calculate_celsius(fahrenheirt)
+    display_celsius(celsius,fahrenheirt)
